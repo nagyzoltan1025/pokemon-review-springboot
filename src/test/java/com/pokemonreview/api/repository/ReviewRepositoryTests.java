f319674c4f84178291b58f7bc1a300133855526b
Nagy Zoltán DEB <nagy.zoltan2@sonrisa.hu>
2025-11-10 09:48:15 +0100

    Implement ReviewRepositoryTests.

---
+++ src/test/java/com/pokemonreview/api/repository/ReviewRepositoryTests.java
@@ -0,0 +1,90 @@
+package com.pokemonreview.api.repository;
+
+import com.pokemonreview.api.models.Pokemon;
+import com.pokemonreview.api.models.Review;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+import java.util.Optional;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+class ReviewRepositoryTests {
+
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    public ReviewRepositoryTests(ReviewRepository reviewRepository) {
+        this.reviewRepository = reviewRepository;
+    }
+
+    @Test
+    void ReviewRepository_SaveAll_ReturnsSavedReview() {
+        Review review = Review.builder().title("title").content("content").stars(5).build();
+
+        Review savedReview = reviewRepository.save(review);
+
+        Assertions.assertThat(savedReview).isNotNull();
+        Assertions.assertThat(savedReview.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    void ReviewRepository_GetAll_ReturnMoreThenOneReview() {
+        Review review1 = Review.builder().title("title").content("content").stars(5).build();
+        Review review2 = Review.builder().title("title").content("content").stars(5).build();
+
+        reviewRepository.save(review1);
+        reviewRepository.save(review2);
+
+        List<Review> reviewList = reviewRepository.findAll();
+
+        Assertions.assertThat(reviewList)
+                .isNotNull()
+                .hasSize(2);
+    }
+
+    @Test
+    void ReviewRepository_FindById_ReturnsSavedReview() {
+        Review review = Review.builder().title("title").content("content").stars(5).build();
+
+        reviewRepository.save(review);
+
+        Review fetchedReview = reviewRepository.findById(review.getId()).get();
+
+        Assertions.assertThat(fetchedReview).isNotNull();
+    }
+
+    @Test
+    void ReviewRepository_UpdateReview_ReturnReview() {
+        Review review = Review.builder().title("title").content("content").stars(5).build();
+
+        reviewRepository.save(review);
+        Review fetchedReview = reviewRepository.findById(review.getId()).get();
+        fetchedReview.setTitle("new title");
+        reviewRepository.save(fetchedReview);
+
+        List<Review> reviewList = reviewRepository.findAll();
+
+        Assertions.assertThat(reviewList)
+                .isNotNull()
+                .hasSize(1);
+        Assertions.assertThat(fetchedReview.getTitle()).isEqualTo("new title");
+    }
+
+    @Test
+    void PokemonRepository_PokemonDelete_ReturnPokemonIsEmpty() {
+        Review review = Review.builder().title("title").content("content").stars(5).build();
+
+        reviewRepository.save(review);
+
+        reviewRepository.deleteById(review.getId());
+        Optional<Review> reviewReturn = reviewRepository.findById(review.getId());
+
+        Assertions.assertThat(reviewReturn).isEmpty();
+    }
+}
